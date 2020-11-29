97cb7982c91083a46ab3834846f64dd8559daa11
masonstoneman <73145558+masonstoneman@users.noreply.github.com>
2020-11-29 16:11:06 -0500

    Initial commit of source file
    Source file just has the basic framework done.

---
+++ Mason_Stoner_Exceptions.cpp
@@ -0,0 +1,18 @@
+// Mason Stoner
+//CIS 1202
+// 11/30/2020
+
+#include <iostream>
+
+using namespace std;
+
+char character(char, int);
+
+int main()
+{
+   
+}
+
+char character(char start, int offset) {
+
+}

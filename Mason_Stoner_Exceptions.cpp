3e436f270d52bbadfee0b5fc377a3c81454b38dd
masonstoneman <73145558+masonstoneman@users.noreply.github.com>
2020-11-29 16:34:10 -0500

    Update Mason_Stoner_Exceptions.cpp
    This is updated with the driver in the main function completed and the first attempt at the exception handling

--- Mason_Stoner_Exceptions.cpp
+++ Mason_Stoner_Exceptions.cpp
@@ -10,9 +10,25 @@ char character(char, int);
 
 int main()
 {
-   
+	character('a', 1);
+	character('a', -1);
+	character('Z', -1);
+	character('?', 5);
 }
 
 char character(char start, int offset) {
 
+	char c;
+
+	if (isalpha(start))
+	{
+		start + offset = c;
+		if (isalpha(c))
+		{
+			cout << c << endl;
+		}
+		else cout << "invalidRangeExpedition" << endl;
+		
+	}
+	else cout << "invalidCharaterException" << endl;
 }
